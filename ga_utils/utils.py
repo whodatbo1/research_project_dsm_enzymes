688341608773a1d18a40699072cde6d60d60a182
Marko Ivanov <marco_ivanov@abv.bg>
2022-06-01 10:20:44 +0300

    Method to check schedule validity

--- ga_utils/utils.py
+++ ga_utils/utils.py
@@ -1,5 +1,5 @@
 import importlib.util
-
+import pandas as pd
 
 def get_instance_info(i):
     fileName = 'FJSP_' + str(i)
@@ -15,3 +15,21 @@ def calculate_problem_size(instance):
         for op in instance.operations[j]:
             init *= len(instance.machineAlternatives[j, op])
     return init
+
+
+def check_valid(schedule: pd.DataFrame):
+    s = schedule.copy(deep=True)
+    s.sort_values(by=['Machine', 'Completion'], ignore_index=True, inplace=True)
+    starts = pd.concat([pd.Series([0]), s['Completion']], ignore_index=True)
+
+    s['Diff'] = s['Start'] - starts
+    s['match'] = s.Machine.eq(s.Machine.shift(fill_value=0))
+    s['check'] = (s.match * s.Diff) >= -0.1
+
+    c = s.check.all()
+
+    if not c:
+        print(c)
+        print(s)
+
+    return c
